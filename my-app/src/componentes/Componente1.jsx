bd2287f0f960b6ec6edea23ad7584424f2af5334
LucasMoraisZago <lucasmoraiszago@hotmail.com>
2022-03-05 13:34:49 +0000

    05/03/2022

--- my-app/src/componentes/Componente1.jsx
+++ my-app/src/componentes/Componente1.jsx
@@ -7,4 +7,13 @@ import React from "react";
 
 // export default () => <h1>Nosso componente Arrow </h1>
 
-export default () => (5+7)
+// export default () => (5+7)
+
+export default (props) =>
+
+    <div>
+        <h1>
+            {props.valor}
+        </h1>
+        <h2>{props.soma}</h2>
+    </div>
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,22 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
 import  Componente1  from "./componentes/Componente1";
+import  {CompA, CompB as B}  from "./componentes/DoisComponentes";
+import  {Teste1,Teste2,Teste3}  from "./componentes/MultiElementos";
+
+
 
 const element = document.getElementById('root')
 
 ReactDOM.render(
     <div>
-        <Componente1/>
+        {/* <Componente1 valor="Olá pessoal" soma={3+4}/> */}
+        {/* <CompA valor="Primeiro componente"/>
+        <B valor="Segundo componente"/> */}
+        {/* <MultiElementos/> */}
+        <Teste1/>
+        <Teste2/>
+        <Teste3/>
     </div>
     ,element
 )
